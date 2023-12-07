96600b2253d14030f084c58bc5317496c47b69d7
NataliaMahera <116887958+NataliaMahera@users.noreply.github.com>
2023-12-07 08:50:43 +0100

    fix

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -15,10 +15,10 @@ type AllType = {
 // створюємо новий тип методом Pick, який звязує логіку між собою, базується на існуючих ключах
 // поля обєктів top, bottom належать-(інструкція перевірки приналежності типів keyof) типу AllType
 
-function compare<T extends AllType, B extends AllType>(
-  top: Pick<T, keyof AllType>,
-  bottom: Pick<B, keyof AllType>
-): AllType {
+function compare<
+  T extends Pick<AllType, "name" | "color">,
+  U extends Pick<AllType, "position" | "weight">
+>(top: T, bottom: U): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -26,5 +26,17 @@ function compare<T extends AllType, B extends AllType>(
     weight: bottom.weight,
   };
 }
+
+// function compare<T extends AllType, B extends AllType>(
+//   top: Pick<T, keyof AllType>,
+//   bottom: Pick<B, keyof AllType>
+// ): AllType {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   };
+// }
 
 export {};
